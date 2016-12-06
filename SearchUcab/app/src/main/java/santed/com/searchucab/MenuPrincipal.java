fab04ab2d3840e2476b520cc18df0c06de073eec
svillami <Sandra villamizar>
2016-12-06 18:17:09 -0430

    Menu Lateral
    Se incorpora parte del menú lateral, falta agregar estilos y mejorar
    algunas cosas.

--- SearchUcab/app/src/main/java/santed/com/searchucab/MenuPrincipal.java
+++ SearchUcab/app/src/main/java/santed/com/searchucab/MenuPrincipal.java
@@ -3,11 +3,92 @@ package santed.com.searchucab;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+
 public class MenuPrincipal extends AppCompatActivity {
+
+    String TITLES[] = {"Buscador","Realidad Aumentada 3D","Guia de Usuario","Tips","Busqueda NFC","Salir"};
+    int ICONS[] = {R.drawable.buscar,R.drawable.buscar,R.drawable.buscar,R.drawable.buscar,R.drawable.buscar,R.drawable.salida};
+
+
+
+
+    private Toolbar toolbar;
+
+    RecyclerView mRecyclerView;
+    RecyclerView.Adapter mAdapter;
+    RecyclerView.LayoutManager mLayoutManager;
+    DrawerLayout Drawer;
+
+    ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mAdapter = new MyAdapter(TITLES,ICONS);
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        mLayoutManager = new LinearLayoutManager(this);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        Drawer = (DrawerLayout) findViewById(R.id.DrawerLayout);
+        mDrawerToggle = new ActionBarDrawerToggle(this,Drawer,toolbar,R.string.openDrawer,R.string.closeDrawer){
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+
+            }
+
+
+
+        }; // Drawer Toggle Object Made
+        Drawer.addDrawerListener(mDrawerToggle); // Drawer Listener set to the Drawer toggle
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
