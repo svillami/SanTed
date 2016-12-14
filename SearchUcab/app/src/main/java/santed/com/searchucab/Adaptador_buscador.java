d08a9ea7b8be00faf412f931092c44203edd6b24
svillami <Sandra villamizar>
2016-12-14 18:24:53 -0430

    Interfaz de las Listas
    Se incorpora la interfaz para el despliegue de las listas

---
+++ SearchUcab/app/src/main/java/santed/com/searchucab/Adaptador_buscador.java
@@ -0,0 +1,13 @@
+package santed.com.searchucab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Adaptador_buscador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adaptador_buscador);
+    }
+}
