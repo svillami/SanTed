723ea7117fbd4c06d5a96b03072afeb88c008f41
Sandra L. Villamizar M <sandravillamiza87@gmail.com>
2017-02-17 18:11:07 -0430

    Boton en Detalle de AR3D
    Se implementa el boton que levanta la Ar3D, en el toolbar del detalle.

--- SearchUcab/app/src/main/java/samples/SamplePoiDetailActivity.java
+++ SearchUcab/app/src/main/java/samples/SamplePoiDetailActivity.java
@@ -1,6 +1,8 @@
 package samples;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
@@ -10,15 +12,21 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import santed.com.searchucab.MenuPrincipal;
 import santed.com.searchucab.R;
 
-public class SamplePoiDetailActivity extends AppCompatActivity {
+public class SamplePoiDetailActivity extends AppCompatActivity  {
 
 	public static final String EXTRAS_KEY_POI_ID = "id";
 	public static final String EXTRAS_KEY_POI_TITILE = "title";
 	public static final String EXTRAS_KEY_POI_DESCR = "description";
+
+	public static final String OPCION = "OPCION";
+	String text = "RA3D";
 
 	private Toolbar toolbar;
 
@@ -33,6 +41,8 @@ public class SamplePoiDetailActivity extends AppCompatActivity {
 		toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
 		setSupportActionBar(toolbar);
 		ActionBar actionBar = getSupportActionBar();
+
+
 		//actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
 		//actionBar.setDisplayHomeAsUpEnabled(true);
 
@@ -41,7 +51,7 @@ public class SamplePoiDetailActivity extends AppCompatActivity {
 
 		//((TextView)findViewById(R.id.poi_id)).setText(  getIntent().getExtras().getString(EXTRAS_KEY_POI_ID) );
 		((TextView)findViewById(R.id.poi_title)).setText( getIntent().getExtras().getString(EXTRAS_KEY_POI_TITILE) );
-		((TextView)findViewById(R.id.poi_description)).setText(  getIntent().getExtras().getString(EXTRAS_KEY_POI_DESCR) );
+		((TextView)findViewById(R.id.poi_description)).setText(getIntent().getExtras().getString(EXTRAS_KEY_POI_DESCR));
 	}
 
 	//Menu para implementar los valores del Toolbar
@@ -49,7 +59,7 @@ public class SamplePoiDetailActivity extends AppCompatActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_info, menu);
+		getMenuInflater().inflate(R.menu.menu_ar3d, menu);
 		return true;
 	}
 
@@ -73,6 +83,16 @@ public class SamplePoiDetailActivity extends AppCompatActivity {
 			case R.id.action_set:
 				//Llamada para la información
 				createSimpleDialog();
+
+				return true;
+
+			case R.id.action:
+				//Llamada para la información
+				//createSimpleDialog();
+				Intent p = new Intent(SamplePoiDetailActivity.this, SampleCamActivity.class);
+				p.putExtra(OPCION,text);
+				startActivity(p);
+
 				return true;
 			/*case R.id.action_sal:
 				//Llamada para salir de la app
@@ -101,6 +121,7 @@ public class SamplePoiDetailActivity extends AppCompatActivity {
 		//El constructor crea el dialogo ya mostrado
 		return builder.create();
 	}
+
 
 
 }
