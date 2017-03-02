a6635cbcb3cd6c53a3bbadfd77bce8ffc4419097
Sandra L. Villamizar M <sandravillamiza87@gmail.com>
2017-03-02 19:04:23 -0430

    Imagenes Informacion AR3D
    Se incorporan las imagenes para la informacion del AR3D

--- SearchUcab/app/src/main/assets/samples/7_Video_4_Bonus-Transparent$Video/js/transparentvideo.js
+++ SearchUcab/app/src/main/assets/samples/7_Video_4_Bonus-Transparent$Video/js/transparentvideo.js
@@ -94,7 +94,7 @@ var World = {
 			Note that this time we use "*" as target name. That means that the AR.Trackable2DObject will respond to any target that is defined in the specified tracker. You can use wildcards to specify more complex name matchings. E.g. 'target_?' to reference 'target_1' through 'target_9' or 'target*' for any targets names that start with 'target'.
 		*/
 
-		var nombres = new Array("4","8");
+		var nombres = new Array("1f","2f", "3f", "1v", "1i");
 
 		for (i=0; i < nombres.length; i++)
 		{
@@ -118,8 +118,8 @@ var World = {
             				if (this.video == null)
             				{
 
-
-            					if (this.targetName == "4")
+								//Video Escuela Informatica
+            					if (this.targetName == "1v")
 								{
 								  this.video = new AR.VideoDrawable("assets/video.mp4", 0.7, {
 									offsetX: -0.2,
@@ -128,7 +128,9 @@ var World = {
 								});
 
 								}
-								else
+
+								//Video transparente con la informacion del Monumento
+								else (this.targetName == "1i")
 								{
 									this.video = new AR.VideoDrawable("assets/transparentVideo.mp4", 0.7, {
 										offsetX: -0.2,
@@ -157,6 +159,7 @@ var World = {
 
 
 								}
+
 
 								this.drawables.addCamDrawable(this.video);
 								if (this.boton != null)
